a641a466d8b33636c63eadfb076ae87de1965ef1
Mariokings <ozorkingsley168@yahoo.com>
2022-09-20 21:32:45 -0700

    done

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("ob"):str.find("g")+8]+str[str.rfind(" w"):str.rfind("h")+1]+" "+str[str.find("P"):str.find("n")+1]
+str = str[39:66] + str[106:111] + " " + str[0:6]
 print(str)
