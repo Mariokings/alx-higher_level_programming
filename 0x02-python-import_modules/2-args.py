a867403ae2b77c0bbb2863208645e45463a6d3aa
Mariokings <ozorkingsley168@yahoo.com>
2022-09-22 16:27:39 -0700

    check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
 
-    check = 1;
+    check = 1
     count = len(argv)
     if count == 1:
-        print("{:d} arguments.".format(count- 1))
+        print("{:d} arguments.".format(count - 1))
     elif count == 2:
         print("{:d} argument:".format(count - 1))
         print("{:d}: {:s}".format(count - 1, argv[count - 1]))
