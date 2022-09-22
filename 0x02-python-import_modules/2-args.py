1b911cfd7151004c79223668eb67d4abe207a534
Mariokings <ozorkingsley168@yahoo.com>
2022-09-22 16:24:29 -0700

    check

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    check = 1;
+    count = len(argv)
+    if count == 1:
+        print("{:d} arguments.".format(count- 1))
+    elif count == 2:
+        print("{:d} argument:".format(count - 1))
+        print("{:d}: {:s}".format(count - 1, argv[count - 1]))
+    else:
+        print("{:d} arguments:".format(count - 1))
+        for i in range(1, count):
+            print("{:d}: {:s}".format(i, argv[check]))
+            check += 1
