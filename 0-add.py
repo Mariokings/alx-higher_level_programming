cbaf8f69877eb8c8295c25568697bee22508c563
Mariokings <ozorkingsley168@yahoo.com>
2022-09-22 23:39:09 -0700

    done

--- 0-add.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-from add_0 import add
-print("{} + {} = {}".format(1, 2, add(1, 2)))
--- practise/101-remove_char_at.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    index = 0
-    for i in str:
-        if index != n:
-            alphabet = i
-        index += 1
