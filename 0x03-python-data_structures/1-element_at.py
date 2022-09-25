dc6b26eb05622a6f69357c53ddb3389924d93a6d
Mariokings <ozorkingsley168@yahoo.com>
2022-09-25 11:00:42 -0700

    check

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,9 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return (None)
-
+        return
     mario = len(my_list) - 1
-
     if idx > mario:
-        return (None)
-
+        return
     kc = my_list.pop(idx)
-    return (kc)
+    return
