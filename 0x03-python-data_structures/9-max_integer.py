d3bbbf705cf68cb939892f621a29eb7d438edb7d
Mariokings <ozorkingsley168@yahoo.com>
2022-09-25 23:00:30 -0700

    check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,6 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         mario = None
     else:
-       my_list.sort()
-       mario = my_list[-1]
+        my_list.sort()
+        mario = my_list[-1]
     return mario
-
