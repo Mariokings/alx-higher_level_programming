11011afe3ae21592e83ac7d6b54dee5b2297784b
Mariokings <ozorkingsley168@yahoo.com>
2022-09-25 21:50:15 -0700

    chech

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 1:
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_a[1] + 0
-        mario = (a, b)
-    elif len(tuple_b) == 0:
-        a = tuple_a[0] + 0
-        b = tuple_a[1] + 0
-        mario = (a, b)
+    if len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    elif len(tuple_a) == 0:
+        a = 0
+        b = 0
     else:
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_a[1] + tuple_b[1]
-        mario = (a, b)
+        a = tuple_a[0]
+        b = tuple_a[1]
+    
+    if len(tuple_b) == 1:
+        a1 = tuple_b[0]
+        b1 = 0
+    elif len(tuple_b) == 0:
+        a1 = 0
+        b1 = 0
+    else:
+        a1 = tuple_b[0]
+        b1 = tuple_b[1]
+    k = a + a1
+    c = b + b1
+    mario = (k, c)
     return mario
