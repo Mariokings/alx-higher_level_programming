aec3020a8c2349f434974ce437f74d59db3f4c23
Mariokings <ozorkingsley168@yahoo.com>
2022-09-25 22:38:35 -0700

    check

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
-    if a == 0:
+
+    if (a == 0):
         mario = (a, None)
     else:
-        mario = (a, b)
+        mario = (a, sentence[0])
+
     return (mario)
