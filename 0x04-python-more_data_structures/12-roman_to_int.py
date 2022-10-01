ae12bf589705c5e1b18971f63f514b75db0ded8c
Mariokings <ozorkingsley168@yahoo.com>
2022-10-01 05:31:29 -0700

    check

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    i,v,x,l,c,d,m = 1,5,10,50,100,500,1000
+    count = mario = check = 0
+    for q in roman_string:
+        if q == "I":
+            num = i
+        elif q == "V":
+            num = v
+        elif q == "X":
+            num = x
+        elif q == "L":
+            num = l
+        elif q == "C":
+            num = c
+        elif q == "D":
+            num = d
+        else:
+            num = m
+        if count >= 1 and check < num:
+            kc = num - check - check
+            check = num
+            count += 1
+            mario += kc
+            continue
+        count += 1
+        check = num
+        mario += num
+    return mario
