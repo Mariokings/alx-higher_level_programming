8b4f5b2d3d7a89dbecdbf50d9a53dc3d25607bc1
Mariokings <ozorkingsley168@yahoo.com>
2022-10-01 08:16:23 -0700

    check

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    mario = []
-    for i in matrix.copy():
-        mario.append(list(map(lambda x: x*x, i)))
-    return mario
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
