59ec730b339c329460def54e70dc4452146983fd
Mariokings <ozorkingsley168@yahoo.com>
2022-10-13 15:11:58 -0700

    check

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+def safe_print_division(a, b):
+    count = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        count += 1
+    finally:
+        if count == 1:
+            print("Inside result: {}".format(None))
+            return None
+        else:
+            print("Inside result: {}".format(result))
+            return result
