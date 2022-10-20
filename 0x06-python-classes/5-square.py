8407d6237a0e5f5e4ed7e3d2a7d8b99ca208be60
Mariokings <ozorkingsley168@yahoo.com>
2022-10-20 07:58:28 -0700

    check

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Square class definition """
+
+
+class Square:
+    """Represents a square class
+    Attributes:
+        size (int): size of a side of he square
+    """
+    def __init__(self, size=0):
+        """Initializing a square
+        Args:
+            size (int): size of a side of the square
+        Returns: current square area
+        """
+        try:
+            assert type(size) == int
+        except AssertionError:
+            raise TypeError("size must be an integer")
+        try:
+            assert size >= 0
+        except AssertionError:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            assert type(value) == int
+        except AssertionError:
+            raise TypeError("size must be an integer")
+        try:
+            assert value >= 0
+        except AssertionError:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
