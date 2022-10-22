163835e9ad70599e75da2cce8ce779b64e59dba2
Mariokings <ozorkingsley168@yahoo.com>
2022-10-22 10:24:21 -0700

    check

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -47,12 +47,37 @@ class SinglyLinkedList:
     """Represents a SinglyLinkedList"""
 
     def __init__(self, head):
-        """Initializing a SinglyLinkedList
+        """Initializing a SinglyLinkedList"""
 
-        Args:
-            head:
-        """
         self.__head = head
 
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+            The node is inserted into the list at the correct
+            ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
+        """
+
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            temp = self.__head
+            while temp.next_node is not None and temp.next_node.data < value:
+                temp = temp.next.node
+            new.next_node = temp.next_node
+            temp.next_node = new
+
     def __str__(self):
-        return f"{self.__head}"
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while temp is not None:
+            values.append(str(temp.data))
+            temp = temp.next_node
+        return ('\n'.join(values))
